ce3dabc2eb37d38ffb8a5f055550b196e097a28c
Gurpreet Sidhu <sidhu@gurpreets-air.lan>
2023-06-25 18:46:05 -0400

    test

--- pbxstores/src/IksComp.js
+++ pbxstores/src/IksComp.js
@@ -8,7 +8,6 @@ export const IksComp = (props) => (
     module="./ItemSearchUIWithStore"
     compProps={props}
     mfeName="item-lookup"
-    // fallbackUrl="https://carbon.6332.lowes.com/omnia/item-keyword-search/remoteEntry.js" // optional
     // loader="Please wait while Loading... " // optional
     // ErrorComponent={() => <ErrorComponent />} // optional
     // LoaderComponent={() => <LoaderComponent />} // optional
