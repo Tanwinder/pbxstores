62e4c0cd0f33e53c3e5d433ce9a2db0041bf4b32
Gurpreet Sidhu <sidhu@gurpreets-air.lan>
2023-10-07 17:20:16 -0400

    remove folder

--- pbxstores/.babelrc.js
+++
@@ -1,7 +0,0 @@
-let modules = process.env.BABEL_ENV === 'commonjs' ? 'commonjs' : false;
-module.exports = {
-  presets: [['@babel/preset-env', { modules }], '@babel/preset-react'],
-  plugins: [
-    '@babel/plugin-proposal-class-properties'
-  ],
-};
